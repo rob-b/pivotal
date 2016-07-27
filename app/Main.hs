955bc530f4aa355780291fcb58a9615ece74bc41
Rob Berry <rob@luvhate.us>
2016-07-27 22:13:33 +0100

    stories command to show unstarted stories

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@
 module Main where
 
 import           Lib                   ( defaultOptions, myProjects
+                                       , stories
                                        , setToken )
 import qualified Data.Text.IO          as TIO
 import           System.Environment    ( lookupEnv )
 import           System.Exit           ( ExitCode(ExitFailure), exitWith )
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy.Char8 as C
 
 main :: IO ()
 main = do
@@ -16,5 +18,5 @@ main = do
       TIO.putStrLn "Must set PIVOTAL_TOKEN"
       exitWith $ ExitFailure 1
     Just token' -> do
-      result <- myProjects $ setToken (BC.pack token') defaultOptions
+      result <- stories $ setToken (BC.pack token') defaultOptions
       TIO.putStrLn result
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,7 @@
 module Lib
     ( me
     , myProjects
+    , stories
     , defaultOptions
     , setToken
     ) where
@@ -15,11 +16,11 @@ import           Network.Wreq            ( Options, Response, checkStatus
 import           Control.Lens
 import           Data.Aeson.Lens         ( _Array, _Integer, _String, key )
 
-import qualified Data.Text.Lazy          as TL
-import qualified Data.Text.Lazy.Encoding as TL
-import qualified Data.Text               as T
-import qualified Data.ByteString         as B
-import qualified Data.ByteString.Lazy    as L
+import qualified Data.Text.Lazy             as TL
+import qualified Data.Text.Lazy.Encoding    as TL
+import qualified Data.Text                  as T
+import qualified Data.ByteString            as B
+import qualified Data.ByteString.Lazy       as L
 
 -- | get info about the authenticated user
 me :: Options -> IO T.Text
@@ -48,11 +49,26 @@ myProjects options = do
     handle200 body = T.append "The names and ids of your projects:\n" $
         T.intercalate "\n" (map formatLine (projectNames body))
 
+stories :: Options -> IO T.Text
+stories options = do
+  r <- getWith options "https://www.pivotaltracker.com/services/v5/projects/xxxxxxx/stories?with_state=unstarted"
+  case (r ^. responseStatus . statusCode) of
+      401 -> return $ handle401 (r ^. responseBody)
+      200 -> return $ handle200 (r ^. responseBody)
+      _ -> return $ decode (r ^. responseBody)
+  where
+    handle200 :: L.ByteString -> T.Text
+    handle200 body = (T.intercalate "\n" . formatStoryDetails . storyDetails) body
 
-simpleApiRequest :: Response L.ByteString -> IO T.Text
-simpleApiRequest r = case (r ^. responseStatus . statusCode) of
-    401 -> return $ handle401 (r ^. responseBody)
-    _ -> return $ decode (r ^. responseBody)
+    storyDetails :: L.ByteString -> [(T.Text, T.Text, T.Text)]
+    storyDetails r = r ^.. _Array . traverse .
+        to (\o -> ( o ^?! key "name" . _String
+                  , o ^?! key "current_state" . _String
+                  , o ^?! key "story_type" . _String
+                  ))
+
+    formatStoryDetails :: [(T.Text, T.Text, T.Text)] -> [T.Text]
+    formatStoryDetails details = map (\(a, b, c) -> (c `T.append` " - " `T.append` a `T.append` " [" `T.append` b `T.append` "]")) details
 
 decode :: L.ByteString -> T.Text
 decode = TL.toStrict . TL.decodeUtf8
@@ -73,3 +89,12 @@ handle401 response = T.intercalate " " [ errorMsg response, possibleFix response
     extract r keyName = r ^. key keyName . _String
     possibleFix r = extract r "possible_fix"
     errorMsg r = extract r "error"
+
+simpleApiRequest :: Response L.ByteString -> IO T.Text
+simpleApiRequest r = case (r ^. responseStatus . statusCode) of
+    401 -> return $ handle401 (r ^. responseBody)
+    _ -> return $ decode (r ^. responseBody)
+
+
+loadSample :: IO L.ByteString
+loadSample = L.readFile "sample.json"
