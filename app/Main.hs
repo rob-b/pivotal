ff33028b7f20978ccf06a954a417e610ec4c997e
Rob Berry <rob@luvhate.us>
2016-07-30 20:14:40 +0100

    Experiment with subparser

--- app/Main.hs
+++ app/Main.hs
@@ -11,13 +11,14 @@ import           System.Environment    ( lookupEnv, getArgs )
 import           System.Exit           ( ExitCode(ExitFailure), exitWith )
 import qualified Data.ByteString.Char8 as BC
 import Data.Maybe (fromMaybe)
+import Options.Applicative
 
 headMaybe :: [a] -> Maybe a
 headMaybe [] = Nothing
 headMaybe (x : _) = Just x
 
-main :: IO ()
-main = do
+main' :: IO ()
+main' = do
     args <- fmap headMaybe getArgs
     let cmd = case fromMaybe "me" args of
             "stories" -> stories
@@ -33,3 +34,36 @@ main = do
         Just token' -> do
             result <- cmd $ setToken (BC.pack token') defaultOptions
             TIO.putStrLn result
+
+
+data Args = Args { aProfile :: Maybe String
+                 , aStories :: Command
+                 } deriving (Show)
+
+data Command = Status String
+             | Stories Integer
+    deriving (Show)
+
+parseInteger :: Parser Integer
+parseInteger = argument auto (metavar "INTEGER")
+
+parseString :: Parser String
+parseString = argument str (metavar "STRING")
+
+profileParser :: ParserInfo Command
+profileParser = info (Status <$> parseString) (progDesc "Display profile info")
+
+storyParser :: ParserInfo Command
+storyParser = info (Stories <$> parseInteger) (progDesc "Display story info")
+
+commandParser :: ParserInfo Command
+commandParser = info commands $ progDesc "My program"
+  where commands = subparser $ mconcat [command "status" profileParser, command "stories" storyParser]
+
+-- options :: Parser Args
+-- options = Args
+--     <$> optional (argument str (metavar "profile"))
+--     <*> commandParser
+main = do
+  cmd <- execParser commandParser
+  print cmd
