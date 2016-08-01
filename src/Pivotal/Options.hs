c8b21b1a4bdf15170144dfd705b7722a3fbb7df4
Rob Berry <rob@luvhate.us>
2016-08-01 21:16:31 +0100

    Handle story detail and story list cli args

--- src/Pivotal/Options.hs
+++ src/Pivotal/Options.hs
@@ -1,12 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Pivotal.Options (execParser, optionsWithInfo, Token, Options(..), Command(..)) where
+module Pivotal.Options
+    ( execParser
+    , optionsWithInfo
+    , Token
+    , Options(..)
+    , Command(..)
+    , StoriesOption(..)
+    ) where
 
 import Options.Applicative
 import qualified Data.ByteString.Char8 as BC
 
-data Options = Options Command deriving (Show)
-data Command = Status String
-             | Stories (Maybe Integer)
+data Options = Options Command
+    deriving (Show)
+
+data StoriesOption = StoriesDetail Integer | StoriesList (Maybe String)
+    deriving (Show)
+
+data Command = Stories StoriesOption
              | Me
              | Projects
     deriving (Show)
@@ -19,21 +30,23 @@ withInfo opts desc = info (helper <*> opts) $ progDesc desc
 optionsWithInfo :: ParserInfo Options
 optionsWithInfo = info (helper <*> parseCommand)
         (fullDesc
-        <> progDesc "Do the thing..."
+        <> progDesc "Interact with pivotal tracker"
         <> header "At the top")
 
 storiesParser :: Parser Command
-storiesParser = Stories <$> optional (argument auto (metavar "story-id"))
+storiesParser = Stories <$> (storiesDetailParser <|> storiesListParser)
 
-statusParser :: Parser Command
-statusParser = Status <$> argument str (metavar "status-kind")
+storiesDetailParser :: Parser StoriesOption
+storiesDetailParser = StoriesDetail <$> argument auto (metavar "story id")
+
+storiesListParser :: Parser StoriesOption
+storiesListParser = StoriesList <$> optional (option str (long "status" <> help "Filter by status" <> metavar "status"))
 
 commandParser :: Parser Command
 commandParser = subparser $
            command "stories" (withInfo storiesParser "View story")
         <> command "profile" (withInfo (pure Me) "View user's profile")
         <> command "projects" (withInfo (pure Projects) "View user's projects")
-        <> command "status" (withInfo statusParser "View stories with given status")
 
 parseCommand :: Parser Options
 parseCommand = Options <$> commandParser
